a8179060f7f39cf500196110ecffde71da036dc2
Jareth <31932204+JustJarethB@users.noreply.github.com>
2024-03-26 00:41:59 +0000

    add anonymous fallback content

--- src/pages/ProfileSelection/index.tsx
+++ src/pages/ProfileSelection/index.tsx
@@ -1,6 +1,7 @@
 import { SignOut } from "@/SignOut";
 import { ProfileSelector } from "@/components/ProfileSelector";
 import { useCurrentUser, useUserProfiles } from "@/api";
+import placeholderPhoto from "@/assets/anon-user.png";
 
 export const ProfileSelection = () => {
   const [user] = useCurrentUser();
@@ -10,9 +11,14 @@ export const ProfileSelection = () => {
     <div className="flex flex-col justify-between py-4">
       <img
         className="block rounded-full w-1/2 mx-auto"
-        src={user.photoURL ?? undefined}
+        src={user.photoURL ?? placeholderPhoto}
+        alt={
+          user.photoURL
+            ? user.displayName ?? "Profile Image"
+            : "Profile Image: Image by gstudioimagen on Freepik"
+        }
       />
-      <p className="text-xl mx-auto">Welcome, {user.displayName}</p>
+      <p className="text-xl mx-auto">Welcome, {user.displayName ?? "User"}</p>
       <div className="border rounded border-neutral-light grid gap-2 grid-cols-2">
         {profiles?.map((profile) => {
           return <ProfileSelector key={profile.churchId} profile={profile} />;
