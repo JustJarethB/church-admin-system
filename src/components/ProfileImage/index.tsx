eedc7391a3d105e10d21d25961f852a94e3eb6dd
Jareth <31932204+JustJarethB@users.noreply.github.com>
2024-04-16 12:50:02 +0100

    add inline prop to ProfileImage

--- src/components/ProfileImage/index.tsx
+++ src/components/ProfileImage/index.tsx
@@ -1,7 +1,20 @@
-type ProfileImageProps = { imageUrl?: string; displayName: string };
-export const ProfileImage = ({ displayName, imageUrl }: ProfileImageProps) => (
+import { twMerge } from "tailwind-merge";
+
+type ProfileImageProps = {
+  imageUrl?: string | null;
+  displayName: string;
+  inline?: boolean;
+};
+export const ProfileImage = ({
+  displayName,
+  imageUrl = null,
+  inline,
+}: ProfileImageProps) => (
   <img
-    className="block rounded-full w-1/2 mx-auto"
+    className={twMerge(
+      "block rounded-full max-h-full max-w-full w-1/2 mx-auto",
+      inline && "size-6"
+    )}
     src={
       imageUrl ??
       `https://api.dicebear.com/8.x/identicon/svg?seed=${displayName}`
