d4629f3b72b4ee3b4c5abacb902fdae6bdc2be6a
Jareth <31932204+JustJarethB@users.noreply.github.com>
2024-04-16 12:50:25 +0100

    add back button to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Error404 = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-2xl pb-16 font-bold">Whoops!</h1>
       <p>Looks like something didn't quite go to plan.</p>
       <p>If you got here by a link, please let us know.</p>
-      <Link className="py-2 text-primary-light font-bold" to="/">
+      <p className="py-2 text-secondary font-bold" onClick={() => navigate(-1)}>
+        Take me back
+      </p>
+      <Link className="py-2 text-primary-light font-bold" to={"/"}>
         Take me home
       </Link>
     </div>
