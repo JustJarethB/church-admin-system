383650e1597124e3b54f6d98c7b3e319329dd135
Jareth <31932204+JustJarethB@users.noreply.github.com>
2024-04-16 12:49:28 +0100

    fix: add user photo back to profile selection

--- src/pages/ProfileSelection/index.tsx
+++ src/pages/ProfileSelection/index.tsx
@@ -9,7 +9,10 @@ export const ProfileSelection = () => {
   if (!user) throw new Error("No user in authenticated zone");
   return (
     <div className="flex flex-col justify-between py-4">
-      <ProfileImage displayName={user.displayName ?? user.uid} />
+      <ProfileImage
+        displayName={user.displayName ?? user.uid}
+        imageUrl={user.photoURL ?? undefined}
+      />
       <p className="text-xl mx-auto">Welcome, {user.displayName ?? "User"}</p>
       <div className="border rounded border-neutral-light grid gap-2 grid-cols-2">
         {profiles?.map((profile) => {
