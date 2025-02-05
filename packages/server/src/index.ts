4d7aca56656417eece889f81c9ac05b467130d2f
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-02-05 13:17:56 +0000

    switch to express

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,21 @@
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
+import express from 'express';
 import { appRouter } from 'api'
-import { createHTTPServer } from '@trpc/server/adapters/standalone'
 import cors from 'cors'
 import { createContext } from 'api'
-const server = createHTTPServer({ router: appRouter, middleware: cors(), createContext })
-
-server.listen(3000)
-console.log("listening on 3000")
+const PORT = 3000
+const app = express()
+app.use(cors());
+app.use(
+    '/',
+    createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }),
+);
+const server = app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    console.log(`listening on`, server.address())
+})
