d029b1258d618f39698f25ce22e89ee5e3a9f7cd
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-02-16 15:52:06 +0000

    default use mobile modal

--- packages/frontend/src/components/Modal/index.tsx
+++ packages/frontend/src/components/Modal/index.tsx
@@ -1,6 +1,7 @@
 import { XMarkIcon } from "@heroicons/react/20/solid"
 import { PropsWithChildren, useState } from "react"
 import { createPortal } from "react-dom"
+import { Button } from "../Button"
 export type ModalProps = PropsWithChildren<{
     onClose: () => void
     title?: string
@@ -9,7 +10,33 @@ export type ModalProps = PropsWithChildren<{
 }>
 export { RoutedModal } from "./Routed"
 
-const MobileModal = () => <div></div>
+const MobileModal = ({ children, onClose, title = "", captive }: ModalProps) => {
+    const handleBackdropClick: React.MouseEventHandler<HTMLDivElement> = ({
+        currentTarget,
+        target,
+    }) => {
+        if (captive) return;
+        if (currentTarget !== target) return;
+        onClose();
+    };
+
+    return (
+        <div onClick={handleBackdropClick} className="absolute top-0 left-0 bottom-0 right-0 z-9 bg-black backdrop-blur-sm bg-opacity-40 p-2">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
+                <div className=" bg-neutral text-neutral-contrast rounded-lg w-full min-h-[50vh] p-4 flex flex-col justify-between gap-4">
+                    <div className="h-8">
+                        <XMarkIcon className="ml-auto text-error hover:text-error-light cursor-pointer h-8" onClick={onClose} />
+                        <h1 className="text-2xl mb-8 text-center font-bold">{title}</h1>
+                    </div>
+                    <div className="">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
 const DesktopModal = ({ children, onClose, title = "", captive }: ModalProps) => {
     const handleBackdropClick: React.MouseEventHandler<HTMLDivElement> = ({
         currentTarget,
@@ -36,7 +63,7 @@ const DesktopModal = ({ children, onClose, title = "", captive }: ModalProps) =>
     )
 }
 export const Modal = (props: ModalProps) => {
-    return createPortal(<DesktopModal {...props} />, document.body)
+    return createPortal(<MobileModal {...props} />, document.body)
 }
 export const useModalController = () => {
     const [isOpen, setIsOpen] = useState(false)
