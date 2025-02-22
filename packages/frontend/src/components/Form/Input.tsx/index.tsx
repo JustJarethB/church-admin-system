d3c5f764d4b8b818fbb8e3e9fc92cf35c9615949
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-02-22 14:58:51 +0000

    readonly input shows like text

--- packages/frontend/src/components/Form/Input.tsx/index.tsx
+++ packages/frontend/src/components/Form/Input.tsx/index.tsx
@@ -1,21 +1,30 @@
 import { twMerge } from "tailwind-merge";
 
-export type InputProps = JSX.IntrinsicElements["input"];
+export type InputProps = JSX.IntrinsicElements["input"] & {
+  label: string
+}
+
+
 export const Input = (props: InputProps) => (
   <div className="group relative text-neutral">
     <input
       required
       {...props}
       className={twMerge(
-        "peer py-4 w-full rounded-xl disabled:bg-opacity-50 bg-neutral-contrast px-8 text-sm outline-none",
+        !props.readOnly && "peer py-4 w-full rounded-xl disabled:bg-opacity-50 bg-neutral-contrast px-8 text-sm outline-none",
+        props.readOnly && " py-4 w-full px-8 text-sm outline-none bg-[inherit] text-white",
         props.className
       )}
     />
     <label
       htmlFor={props.id}
-      className="absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 group-focus-within:-top-7 group-focus-within:h-1/2 group-focus-within:pl-0 group-focus-within:text-base group-focus-within:text-neutral-contrast peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-neutral-contrast"
+      className={twMerge(
+        !props.readOnly && "absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 group-focus-within:-top-7 group-focus-within:h-1/2 group-focus-within:pl-0 group-focus-within:text-base group-focus-within:text-neutral-contrast peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-neutral-contrast",
+        props.readOnly && "absolute left-2 flex transform items-center transition-all duration-300 -top-7 h-1/2 pl-0 text-base text-neutral-contrast peer-valid:-top-7 peer-valid:h-1/2 peer-valid:pl-0 peer-valid:text-base peer-valid:text-neutral-contrast",
+      )}
     >
-      {props.id}
+      {props.label}
     </label>
   </div>
 );
+
