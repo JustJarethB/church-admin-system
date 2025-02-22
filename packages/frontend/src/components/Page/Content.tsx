0c498287c380f93ba564a2a0b0e72f1902619b24
Jareth Bower <31932204+JustJarethB@users.noreply.github.com>
2025-02-22 14:59:11 +0000

    Content padding change

--- packages/frontend/src/components/Page/Content.tsx
+++ packages/frontend/src/components/Page/Content.tsx
@@ -2,5 +2,5 @@ import { twMerge } from "tailwind-merge";
 import { withSuspense } from "../Suspended";
 
 export const Content = withSuspense((props: JSX.IntrinsicElements["div"]) => (
-  <div {...props} className={twMerge("p-2 ", props.className)} />
+  <div {...props} className={twMerge("p-2 mt-8", props.className)} />
 ));
